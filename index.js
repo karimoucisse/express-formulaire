509fdceb3971159a18dd2f04a260bfb94fb5bd9a
karimou cisse <karimou.cisse1@gmail.com>
2021-12-08 10:12:02 +0100

    commit

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ express = require ("express");
 app = express()
 const port = 5000;
 
+const engine = require("express-Handlebars").engine
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+
+app.use(express.urlencoded({ extended : true }))
+
+app.get('/', (req, res) => {
+    res.render('formulaire')
+})
+app.post('/signup', (req,res) => {
+    res.send(`bienvenue ${req.body.username}`)
+    console.log(req.body)
+})
+
+
+
 app.listen(port, () => {
     console.log(`serveur is running on ${port}`);
 })
 
-app.get('/', (req,res) => {
-    res.send('home')
-})
+
+
+
+
+
